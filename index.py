42ced33534e129ff4b91f3dc594ba14a4497ae53
Josh Nixon <joshuanixon115@gmail.com>
2017-10-13 15:55:04 +0100

    Added public messaging code

--- index.py
+++ index.py
@@ -1,23 +1,93 @@
 import talalib
+import sqlite3
+import threading
+import random
+import string
+import datetime
+import json
+
+conn = sqlite3.connect("student_database.db")
+c = conn.cursor()
+
+tables = [
+    {
+        "name": "memos",
+        "columns": "memo_name TEXT, memo_data TEXT"
+    },
+    {
+        "name": "config",
+        "columns": "pin TEXT"
+]
+
+for table in tables:
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [
+              table["name"]])
+    data = c.fetchall()
+
+    if len(data) <= 0:  # If table doesn't exist
+        print("Creating table " + table["name"])
+        print("CREATE TABLE " + table["name"] + " (" + table["columns"] + ")")
+        c.execute("CREATE TABLE " +
+                  table["name"] + " (" + table["columns"] + ")")
+
 
 tala = talalib.Tala()
 
 tala.clear()
 
+def encode(data):
+    data = json.dumps(data) #Json dump message
+    return(data) 
+def decode(data):
+    data = json.loads(data) #Load from json
+    return(data)
+
+
 while True:
-    choice = tala.menu(["Public Message", "Private Message", "Snake", "Distress Beacon", "Settings", "Power Off"])
+    choice = tala.menu(["Public Message", "Private Message", "Snake", "Memo", "Settings", "Power Off"])
     if choice == "Public Message":
-        message = tala.type()
-        tala.send(message)
-        
+        def recv_data():
+            print("Thread has run")
+
+        thread_recv_data = threading.Thread(target=recv_data):
+        thread_recv_data.start()
+
+        content = tala.type()
+    
+        msg_id = ""
+        i = 0
+        while i != 5:
+            msg_id += random.choice(string.ascii_lowercase)
+    
+        timestamp = datetime.datetime.now()        
+                
+        message = { "content": content,
+                    "id": msg_id, 
+                    "timestamp": timestamp, 
+                    "sender": "", 
+                    "recipient": ""
+                    }
+            
+        tala.send(encode(message))
+	    
     elif choice == "Private Message":
          message = tala.type()
          tala.message("Debug", message) #Replace this with code for sending message
-        
     elif choice == "Snake":
         tala.message("Alert", "[game code here]")
-    elif choice == "Distress Beacon":
-        tala.message("Alert", "[distress beacon code here]")
+	elif choice == "Memo":
+		c.execute("SELECT * FROM memos")
+		memos = c.fetchall()
+		choice = tala.menu(["New Memo"], memos)
+		if choice == "New Memo":
+			memo_data = tala.type()
+			tala.message("Alert", "Input memo name")
+			memo_name = tala.type()
+			c.execute("INSERT INTO memos (memo_name, memo_data) values (?, ?)", [memo_name, memo_data])
+		else:
+			c.execute("SELECT memo_data FROM memos WHERE memo_name = ?", [choice])
+			memo_data = c.fetchall()
+			tala.message(choice, memo_data[0])
     elif choice == "Settings":
         while True:
             choice = tala.menu(["Change Pin", "Reset Device Key", "Exit Options"])
